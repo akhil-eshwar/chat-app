a1275d18915996f6a22b10539e4b8cf18ff90a0c
akhil-eshwar <eshwarakhil948@gmail.com>
2025-06-24 13:58:55 +0530

    final

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,12 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173", // dev
+      "https://chat-app-lime-three.vercel.app" // production frontend
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
